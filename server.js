736a3fe01ee5ca1a0d29bef11b8f5c5e54514df3
george-g <georgegombert@gmail.com>
2020-05-11 15:56:20 -0600

    Removed redundant dependancies

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const path = require("path");
-const noteData = require("./Develop/db/db.json");
-const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
